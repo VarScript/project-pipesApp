c0e9a80ee79fc7dbfb2fd0accd2ea86d44274abe
varscript <fvargas.edu@gmail.com>
2024-05-20 19:58:11 -0500

    primeNG

---
+++ src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styles: ``,
+})
+export class MenuComponent implements OnInit {
+  menuItems: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.menuItems = [
+      {
+        label: 'Pipes de Angular',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Textos y Fechas',
+            icon: 'pi pi-align-left',
+          },
+          {
+            label: 'Números',
+            icon: 'pi pi-dollar',
+          },
+          {
+            label: 'No comunes',
+            icon: 'pi pi-globe',
+          },
+        ],
+      },
+      {
+        label: 'Pipes personalizados',
+        icon: 'pi pi-cog',
+        items:[
+          {
+            label:'Some Here',
+            icon: 'pi pi-cog',
+            items: [
+              {
+                label:'some'
+              }
+            ]
+          }
+        ]
+      }
+    ];
+  }
+}
