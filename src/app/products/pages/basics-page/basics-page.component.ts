8e4a9feaebc56241823f7cdb92058108bab4be00
varscript <fvargas.edu@gmail.com>
2024-05-21 16:41:06 -0500

    date tipe

--- src/app/products/pages/basics-page/basics-page.component.ts
+++ src/app/products/pages/basics-page/basics-page.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-basics-page',
   templateUrl: './basics-page.component.html',
-  styleUrl: './basics-page.component.css'
+  styleUrl: './basics-page.component.css',
 })
 export class BasicsPageComponent {
+  public nameLower = 'fabio';
+  public nameUpper = 'FABIO';
+  public nameTitle = 'FaBIo VArgAs';
 
-public nameLower = 'fabio'
-public nameUpper = 'FABIO'
-public nameTitle = 'FaBIo VArgAs'
-
+  public customDate = new Date();
 }
