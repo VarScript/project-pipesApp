287234a939c734e6ad968ad1ddc91c11821ab5c4
varscript <fvargas.edu@gmail.com>
2024-05-27 13:52:32 -0500

    i18nSelect

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-numbers-page',
   templateUrl: './numbers-page.component.html',
-  styleUrl: './numbers-page.component.css'
+  styleUrl: './numbers-page.component.css',
 })
 export class NumbersPageComponent {
-
+  public totalSells: number = 2344356.234234;
+  public percent: number = 0.4234;
 }
--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrl: './uncommon-page.component.css'
+  styleUrl: './uncommon-page.component.css',
 })
 export class UncommonPageComponent {
+  public name: string = 'Fabio';
+  public genre: 'male' | 'female' = 'male';
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  };
 
+  changeClient(): void {
+    this.name = 'Derly';
+    this.genre = 'female';
+  }
 }
